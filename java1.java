c82da34811911281aec7917fa33a6a52a7f55720
mynt35 <155460615+mynt35@users.noreply.github.com>
2024-01-03 21:17:25 +0700

    add

---
+++ java1.java
@@ -0,0 +1,8 @@
+public class java1 {
+    public static void main(String[] args) {
+        System.out.println("Viettel VTCC");
+        //khai bao
+        String hoten="abc";
+        System.out.println(hoten);
+    }
+}
